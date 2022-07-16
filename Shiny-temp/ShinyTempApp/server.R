71faf2a46b0a9b9d55e49fa2c4e77b66da7a6e0b
Raghav1881 <59401149+Raghav1881@users.noreply.github.com>
2022-07-16 01:07:12 -0400

    holy cow the violin plots finally work
    good job me

--- Shiny-temp/ShinyTempApp/server.R
+++ Shiny-temp/ShinyTempApp/server.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(Seurat)
+library(ggplot2)
 
 geneList <- rownames(dataset)
 diagnosisList <- levels(dataset@meta.data[["diagnosis"]])
@@ -23,7 +24,8 @@ getGeneList <- function(diaglist, celllist) {
 shinyServer(function(input, output, session) {
   updateSelectizeInput(session, "genediag",
                       choices = geneList,
-                      server = TRUE)
+                      server = TRUE,
+                      selected = "C9orf72")
   updateSelectizeInput(session, "diagchk",
                       choices = diagnosisList[-1],
                       server = TRUE)
@@ -31,4 +33,44 @@ shinyServer(function(input, output, session) {
   currentGeneDiag <- reactive({getGeneList(input$diagchk, input$celltype)})
   output$test <- renderPrint({print({currentGeneDiag()[1]})})
   output$test2 <- renderPrint({print({currentGeneDiag()[2]})})
+
+  output$violin1 <- renderPlot({
+    validate(
+      need(input$genediag, "Please select a gene"),
+      need(input$diagchk, "Please select a diagnosis"),
+      need(input$celltype, "Please select the celltypes")
+    )
+    VlnPlot(dataset,
+      features = input$genediag,
+      idents = currentGeneDiag()[[1]],
+      pt.size = 0
+    ) &
+    theme(axis.title.x = element_blank())
+  })
+
+  output$violin2 <- renderPlot({
+    validate(
+      need(input$genediag, ""),
+      need(input$diagchk, ""),
+      need(input$celltype, "")
+    )
+    VlnPlot(dataset,
+      features = input$genediag,
+      idents = currentGeneDiag()[[2]],
+      pt.size = 0
+    ) &
+    theme(axis.title.x = element_blank())
+  })
+
+  output$features_graph <- renderPlot({
+    validate(
+      need(input$feats, "No features selected"))
+    VlnPlot(dataset,
+      features = input$feats,
+      pt.size = 0,
+      ncol = 3,
+      group.by = "group") &
+    theme(axis.title.x = element_blank())
+  })
+
 })
--- Shiny-temp/ShinyTempApp/ui.R
+++ Shiny-temp/ShinyTempApp/ui.R
@@ -4,6 +4,8 @@ library(shiny)
 library(bslib)
 
 mtheme <- bs_theme(version = 4, bootswatch = "minty")
+feature_list <- c("nCount_RNA", "nCount_SCT", "nFeature_RNA",
+                  "nFeature_SCT", "percent.mt", "percent.rpl", "percent.rps")
 
 ui <- fluidPage(theme = mtheme,
   titlePanel(
@@ -24,9 +26,28 @@ ui <- fluidPage(theme = mtheme,
                         "Select diagnosis",
                         choices = NULL),
             checkboxGroupInput("celltype", "Select cell types",
-                                levels(dataset$celltype))),
+                                levels(dataset$celltype),
+                                selected = "Oligodendrocytes")),
           mainPanel(
-            width = 9, verbatimTextOutput("test"), verbatimTextOutput("test2")))),
-      tabPanel("Quality Control")
+            width = 9,
+            fluidRow(
+              verbatimTextOutput("test"), verbatimTextOutput("test2")),
+            fluidRow(
+              column(
+                6, plotOutput("violin1")),
+              column(
+                6, plotOutput("violin2")))))),
+      tabPanel("Quality Control",
+        HTML("Cell Quality Control Metrics"),
+        # Create input for extra features
+        fluidRow(
+          sidebarPanel(
+            # Checkbox input for selecting features
+            width = 3, checkboxGroupInput("feats", "Display extra features",
+                                          feature_list)),
+          # Plot features graphs
+          mainPanel(
+            width = 9, plotOutput("features_graph",
+            height = "80vh"))))
     )
 )
